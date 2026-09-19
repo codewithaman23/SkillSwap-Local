628b30c3b8888f84f3eb12c5cceaaf9614fcdd80
codewithaman23 <aman77aks@gmail.com>
2026-09-19 18:33:48 +0530

    Prepare project for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 3000,
+
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
@@ -14,4 +16,3 @@ export default defineConfig({
     },
   },
 });
-
